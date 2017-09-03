4dfb5be9ce8f6e664398468008c63721f1e760ec
Dylan Mozlowski <dmozlowski@classy.org>
2017-09-03 14:27:15 -0700

    Set up docs website

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `glamorous-grid`,
   },
   plugins: [`gatsby-plugin-react-helmet`],
+  pathPrefix: '/glamorous-grid'
 }
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -3,9 +3,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import { Col } from '../../../src'
+
 const IndexPage = () => (
   <div>
-    <h1>New library</h1>
+    <h1><code>glamorous-grid</code></h1>
+    <Col />
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
