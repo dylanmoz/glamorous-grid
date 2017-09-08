927c11fef59ccde49c98837163656af715d3ad7a
Dylan Mozlowski <dmozlowski@classy.org>
2017-09-08 08:04:35 -0700

    Add google analytics to docs

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -2,6 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'glamorous-grid',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-glamor'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-glamor',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-105846113-1',
+      },
+    }
+  ],
   pathPrefix: '/glamorous-grid'
 }
